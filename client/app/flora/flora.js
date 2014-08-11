8de3f9347b251bbe95dc539f1aad3263682d16e8
Pablo Cid <pabloagronomo@gmail.com>
2014-08-11 12:59:39 -0400

    krap
    Problema con lanzar el servidor

--- client/app/flora/flora.js
+++ client/app/flora/flora.js
@@ -6,5 +6,13 @@ angular.module('fullstack012App')
       .when('/flora', {
         templateUrl: 'app/flora/flora.html',
         controller: 'FloraCtrl'
+      })
+      .when('/flora/:id', {
+        templateUrl: 'app/flora/flora-show.html',
+        controller: 'FloraCtrl'
+      })
+      .when('/flora/:id/edit', {
+        templateUrl: 'app/flora/flora-edit.html',
+        controller: 'FloraCtrl'
       });
   });
