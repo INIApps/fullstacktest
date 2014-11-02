4bfb30207fe5a21aaf20052a9e4bbb01791689d2
Pablo Cid <pabloagronomo@gmail.com>
2014-11-02 15:13:14 -0300

    kjra

---
+++ client/app/tablas/tablas.controller.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Controller: TablasCtrl', function () {
+
+//  // load the controller's module
+//  beforeEach(module('fullstack012App'));
+//
+//  var TablasCtrl, scope;
+//
+//  // Initialize the controller and a mock scope
+//  beforeEach(inject(function ($controller, $rootScope) {
+//    scope = $rootScope.$new();
+//    TablasCtrl = $controller('TablasCtrl', {
+//      $scope: scope
+//    });
+//  }));
+//
+//  it('should ...', function () {
+//    expect(1).toEqual(1);
+//  });
+});
