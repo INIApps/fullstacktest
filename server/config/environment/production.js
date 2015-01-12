6c66e18a8f5acc9ecd7951228ac88f0b9e284afc
Pablo Cid <pablo@MacBook-Pro-de-Pablo.local>
2015-01-12 12:12:33 -0300

    Adicion de coneccion mongodb en production.js

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,5 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            uri: 'mongodb://localhost/fullstack014-dev'
   }
 };
